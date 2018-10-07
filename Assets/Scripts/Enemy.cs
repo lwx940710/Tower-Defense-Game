1af397570ecb8f9e7b7ba7c1006bf73533787067
Wenxue Li <lwx940710@gmail.com>
2018-10-08 07:58:24 +0800

    Up to countdown & wave number update

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 10f;
+    private Transform target;
+    private int waypointIndex;
+
+    private void Start()
+    {
+        target = Waypoints.waypoints[0];
+        waypointIndex = 0;
+    }
+
+    private void Update()
+    {
+        // Update the target if it reaches the current waypoint
+        if (Vector3.Distance(target.position, transform.position) <= 0.2f)
+        {
+            waypointIndex++;
+            // If the enemy reaches the end point, destroy the enemy
+            if (waypointIndex == Waypoints.waypoints.Length)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            target = Waypoints.waypoints[waypointIndex];
+        }
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+        
+    }
+}
---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+    public float timeBetweenWaves = 5.5f;
+    private float countdown;
+    private int waveNo = 1;
+    private bool curWaveSpawned = true;
+
+    public Text countdownText;
+    public Text waveNoText;
+
+    private void Start()
+    {
+        countdown = timeBetweenWaves;
+        countdownText.text = Mathf.Round(countdown).ToString();
+        waveNoText.text = waveNo.ToString();
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+    IEnumerator SpawnWave()
+    {
+        for (int i = 0; i < waveNo; ++i)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.3f);
+        }
+        countdown = timeBetweenWaves;
+        waveNo++;
+        curWaveSpawned = true;
+    }
+
+    private void Update()
+    {
+        if (curWaveSpawned)
+        {
+            countdown -= Time.deltaTime;
+            if (countdown <= 0f)
+            {
+                curWaveSpawned = false;
+                StartCoroutine(SpawnWave());
+            }
+
+            countdownText.text = Mathf.Round(countdown).ToString();
+            waveNoText.text = waveNo.ToString();
+        }
+    }
+}
